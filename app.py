f3b9e0423ef94a3bd33e8b99f78eb7334258031b
Cyra Alesha <cyraanindyaalesha@gmail.com>
2024-10-26 17:51:42 -0400

    flask boilerplate

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Run the Flask app
+if __name__ == '__main__':
+    app.run(debug=True)
