9c1f187de3c8205be023e4fb3dd2804b2fbe67d5
Cyra Alesha <cyraanindyaalesha@gmail.com>
2024-10-26 22:00:05 -0400

    Fix SQL issues and add home page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request, redirect, url_for
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
@@ -8,17 +8,17 @@ app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///users.db'
 
 # Initilising the database
 
-db = SQLALchemy(app)
+db = SQLAlchemy(app)
 
 #Create database tables
 
-#Make two separate tables for doctors and patitents
-class User(db.model):
-    id = db.column(db.Integer, primary_key = True)
-    firstName = db.Column(db.string(200), nullable=False)
-    lastName = db.Column(db.string(200), nullable=False)
-    email = db.Column(db.string(200), nullable=False)
-    password =  db.Column(db.string(200), nullable=False)
+#Make two separate tables for doctors and patients
+class User(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(200), nullable=False) 
+    lastName = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
     #Create a function to return a string when add something
     #Use this function for when needing to show info on profile page name and whether patient or not
