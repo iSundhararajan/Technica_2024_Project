0b593fb3c662e24f3ba76df96c46d787a68a6896
Brittany Chan <brittanyhaileec@gmail.com>
2024-10-26 19:19:17 -0700

    initial uploading of files

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask import Flask, render_template, jsonify, request, redirect, url_for, send_file
+from flask_socketio import SocketIO
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 
+@app.route('/upload', methods=['POST'])
+def uploadFile():
+    if 'file' not in request.files:
+        return "No file part", 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+    if file: 
+        return "Generating hash..."
+    
+
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5000, debug=True)
